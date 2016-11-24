0efefa9e3df53f1f4cbb07193a4e9783af180707
sunyear <relasoft2002@gmail.com>
2016-11-23 23:29:32 -0300

    Nuevas constantes
    Constantes PROCESOS y ESTADOS_LOTES

--- public/js/lotes-constants.js
+++ public/js/lotes-constants.js
@@ -6,6 +6,19 @@
 		.constant('TABLA', {
         		'NOMBRE': 'lotes_'
     		}
-    	);
+    	)
+    	.constant('PROCESOS',{
+    			'ID_PROCESO_PREVAL': 100,
+    			'ID_PROCESO_PRECARGA': 200,
+    			'ID_PROCESO_CALIDAD': 300
+    		}
+    	)
+    	.constant('ESTADOS_LOTES',{
+    			1 : 'ACEPTADO',
+    			2 : 'RECHAZADO',
+    			3 : 'OBSERVADO',
+    			4 : 'EN PROCESO'
+    		}
+    );
 
 })();
